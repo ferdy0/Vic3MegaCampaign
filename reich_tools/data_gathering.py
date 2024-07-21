00ec4bbae5bb8541252992387015aec0107b7f44
Codeminister2137 <kubajaworski99@wp.pl>
2024-07-21 11:57:17 +0200

    Attempt num 2137

--- reich_tools/data_gathering.py
+++ reich_tools/data_gathering.py
@@ -186,7 +186,7 @@ def write_states_file(filename: str, states_container: States):
     states_block_data = states_block_match.group(1)
 
     state_blocks = re.findall(
-        r"s:(\w+)\s*=\s*({.*?})\s*(?=\s*s:|$)", states_block_data, re.DOTALL
+        r"\s*s:(\w+)\s*=\s*({.*?})\s*(?=\s*s:|$)", states_block_data, re.DOTALL
     )
     modified_states_block_data = states_block_data
 
@@ -209,7 +209,7 @@ def write_population_file(filename: str, states_container: States):
         original_data = file.read()
 
     state_blocks = re.findall(
-        r"s:(\w+)\s*=\s*({.*?})\s*(?=\s*s:|$)", original_data, re.DOTALL
+        r"\s*s:(\w+)\s*=\s*({.*?})\s*(?=\s*s:|$)", original_data, re.DOTALL
     )
     modified_data = original_data
 
@@ -290,35 +290,59 @@ def generate_population_data_block(state: State) -> str:
 
 
 def generate_state_data_block(state: State) -> str:
-    block = f"s:{state.name} = {{\n"
-    for region in state.regions:
-        block += "\tcreate_state = {\n"
-        block += f"\t\tcountry = c:{region.country}\n"
-        block += f"\t\towned_provinces = {{ {' '.join(region.provinces)} }}\n"
-        block += f"\t\tstate_type = {region.type}\n"
+    block = f"{state.name} = {{\n"
+    block += f"\tid = {state.id}\n"
+    block += f'\tsubsistence_building = "{state.subsistence_building}"\n'
+
+    # Format provinces list correctly
+    provinces_formatted = " ".join(f'"{prov}"' for prov in state.provinces)
+    block += f"\tprovinces = {{ {provinces_formatted} }}\n"
+
+    if state.traits:
+        traits_formatted = " ".join(f'"{trait}"' for trait in state.traits)
+        block += f"\ttraits = {{ {traits_formatted} }}\n"
+
+    if state.city:
+        block += f'\tcity = "{state.city}"\n'
+
+    if state.port:
+        block += f'\tport = "{state.port}"\n'
+
+    if state.farm:
+        block += f'\tfarm = "{state.farm}"\n'
+
+    if state.mine:
+        block += f'\tmine = "{state.mine}"\n'
+
+    if state.wood:
+        block += f'\twood = "{state.wood}"\n'
+
+    block += f"\tarable_land = {state.arable_land}\n"
+
+    if state.arable_resources:
+        arable_resources_formatted = " ".join(
+            f'"{res}"' for res in state.arable_resources
+        )
+        block += f"\tarable_resources = {{ {arable_resources_formatted} }}\n"
+
+    if state.capped_resources:
+        block += "\tcapped_resources = {\n"
+        for resource, amount in state.capped_resources.items():
+            block += f"\t\t{resource} = {amount}\n"
         block += "\t}\n"
-    for homeland in state.homelands:
-        block += f"\tadd_homeland = cu:{homeland}\n"
-    for claim in state.claims:
-        block += f"\tadd_claim = c:{claim}\n"
+
+    if state.resource:
+        for res in state.resource:
+            block += "\tresource = {\n"
+            block += f'\t\ttype = "{res["type"]}"\n'
+            block += f'\t\tundiscovered_amount = {res["undiscovered_amount"]}\n'
+            block += "\t}\n"
+
+    if state.naval_exit_id:
+        block += f"\tnaval_exit_id = {state.naval_exit_id}\n"
+
     block += "}\n"
     return block
-
-
-def modify_data(states_container: States):
-    for state in states_container.states.values():
-        pop_sum = 0
-        for region in state.regions:
-            pop_sum += region.sum_populations()
-        min_arable_land = pop_sum / 50000
-        if state.arable_land is None:
-            print(state.to_dict())
-        if state.arable_land < min_arable_land:
-            old_arable_land = state.arable_land
-            state.arable_land = min_arable_land
-            print(
-                f"{state.name}: Population = {pop_sum}, changing arable land from {old_arable_land} to {state.arable_land}"
-            )
 
 
 # Example usage
